88cefbf754ff1ac6c27a6b0918170f360986d051
simonky <simonasb19@gmail.com>
2024-03-22 08:15:18 -0300

    base

---
+++ usuarios/src/test/java/com/simonky/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonky.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
