efac566897bac3ca4fad31050f081dc7f1fa79be
simonky <simonasb19@gmail.com>
2024-04-27 12:31:37 -0400

    Pruebas Extras

--- usuarios/src/test/java/com/simonky/usuarios/controller/UsuarioControllerTest.java
+++ usuarios/src/test/java/com/simonky/usuarios/controller/UsuarioControllerTest.java
@@ -1,5 +1,7 @@
 package com.simonky.usuarios.controller;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -13,10 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.simonky.usuarios.exceptions.UsuarioNotFoundException;
 import com.simonky.usuarios.model.Usuario;
 import com.simonky.usuarios.service.UsuarioServiceImpl;
 
@@ -59,6 +64,41 @@ public class UsuarioControllerTest {
     }
 
     @Test
+    public void obtenerUsuarioNoExistenteTest() throws Exception {
+        Long idUsuario = -1L; 
+        when(usuarioServiceMock.getUsuarioById(idUsuario)).thenThrow(new UsuarioNotFoundException(idUsuario));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/usuarios/{id}", idUsuario))
+            .andExpect(MockMvcResultMatchers.status().isNotFound())
+            .andExpect(result -> assertTrue(result.getResolvedException() instanceof UsuarioNotFoundException));
+
+        verify(usuarioServiceMock, times(1)).getUsuarioById(idUsuario);
+    }
+
+    @Test
+    public void obtenerUsuarioExistenteTest() throws Exception {
+        Long idUsuarioExistente = 1L;
+
+        Usuario usuarioExistente = new Usuario();
+        usuarioExistente.setId(idUsuarioExistente);
+        usuarioExistente.setEmail("email@gmail.com");
+        usuarioExistente.setPassword("password123#");
+        usuarioExistente.setNombre("Simon");
+
+        when(usuarioServiceMock.getUsuarioById(idUsuarioExistente)).thenReturn(usuarioExistente);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/usuarios/{id}", idUsuarioExistente))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(idUsuarioExistente.intValue()))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("email@gmail.com"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.nombre").value("Simon"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.password").value("password123#"));
+
+        verify(usuarioServiceMock, times(1)).getUsuarioById(idUsuarioExistente);
+    }
+
+
+    @Test
     public void eliminarUsuarioTest() throws Exception {
         Long idUsuario = 1L;
 
@@ -70,4 +110,27 @@ public class UsuarioControllerTest {
         verify(usuarioServiceMock, times(1)).deleteUsuario(idUsuario);
 
     }
+
+
+    @Test
+    public void crearUsuarioTest() throws Exception {
+        Usuario usuarioNuevo = new Usuario();
+        usuarioNuevo.setEmail("email@gmail.com");
+        usuarioNuevo.setPassword("password123#");
+        usuarioNuevo.setNombre("Simon");
+
+        when(usuarioServiceMock.createUsuario(any(Usuario.class))).thenReturn(usuarioNuevo);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/usuarios")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(usuarioNuevo)))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("email@gmail.com"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.nombre").value("Simon"));
+
+        
+        verify(usuarioServiceMock, times(1)).createUsuario(any(Usuario.class));
+    }
+
+    
 }
